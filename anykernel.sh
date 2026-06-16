c03efebc1e7ddf4dfc415f0192e254c9601fa562
Alexjr2 <52109110+Alexjr2@users.noreply.github.com>
2026-06-16 17:56:40 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KernelSU With SUSFS by Rindaman5555
+kernel.string=KernelSU With by MockGPS
 do.devicecheck=1
 do.modules=0
 do.systemless=1
