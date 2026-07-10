d9d12fd8534a006b80176618e48cefd89173f2b6
Alexjr2 <52109110+Alexjr2@users.noreply.github.com>
2026-07-11 04:31:23 +0800

    Update kernel string in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=KernelSU With by MockGPS
+kernel.string=KernelSU Phoenix by MockGPS
 do.devicecheck=1
 do.modules=0
 do.systemless=1
